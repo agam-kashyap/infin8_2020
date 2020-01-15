e0cb1f7472b7ac3e5bffe49231bfba475473409a
agam-kashyap <agamkashyap2018@gmail.com>
2020-01-15 18:33:24 +0530

    Main page edited

--- src/components/sections/TopSection.js
+++ src/components/sections/TopSection.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { Carousel } from 'antd';
 import '../css/TopSection.css';
+import '../css/TopSection.scss';
+import '../css/bganimation.scss';
 
 class TopSection extends React.Component {
 render() {
  return (
  <Carousel autoplay>
     <div>
+     <div className="header"/>
+        { /* <div class="hero">
+          <div class="hero__title">Hello World</div>
+          <div class="cube"></div>
+          <div class="cube"></div>
+          <div class="cube"></div>
+          <div class="cube"></div>
+          <div class="cube"></div>
+          <div class="cube"></div>
+      </div> */}
       <img src={require('../assets/infin8logofrowebsite.png')} alt=""/>
+      <div class="wrapper">
+        <div class="letters"><span class="letter">c</span><span class="letter">u</span><span class="letter">l</span><span class="letter">t</span><span class="letter">u</span><span class="letter">r</span><span class="letter">a</span><span class="letter">l</span><span class="letter"> </span>
+                <span
+                    class="letter">f</span><span class="letter">e</span><span class="letter">s</span><span class="letter">t</span><span class="letter"> </span><span class="letter">o</span><span class="letter">f</span><span class="letter"> </span><span class="letter">i</span>
+                    <span
+                        class="letter">i</span><span class="letter">i</span><span class="letter">t</span><span class="letter"> </span><span class="letter">b</span><span class="letter">a</span><span class="letter">n</span><span class="letter">g</span><span class="letter">a</span>
+                        <span
+                            class="letter">l</span><span class="letter">o</span><span class="letter">r</span><span class="letter">e</span></div>
+        </div>
+        <div className="watermark"/>
     </div>
   </Carousel>
  );
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import { createBrowserHistory } from "history";
 
 //Pages
 import Home from './components/home';
+import Blank from './components/sections/Blank';
 // import Login from './components/login';
 // import Register from './components/register';
 // import BookingComponent from './components/bookingcomponent';
@@ -15,6 +16,7 @@ export default (
     <Router history={history}>
     <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/blank" component={Blank}/>
         {/* <Route exact path="/login" component={Login}/> */}
         {/* <Route exact path="/register" component={Register}/> */}
         {/* <Route exact path="/booking" component={BookingComponent}/> */}
