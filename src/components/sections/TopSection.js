35e4e57633bbad5d2d78834c71dd9cdd9988c00a
Prajwal7842 <prajwalgoel@gmail.com>
2020-01-15 23:35:41 +0530

    Add Date

--- src/components/sections/TopSection.js
+++ src/components/sections/TopSection.js
@@ -9,14 +9,14 @@ render() {
  return (
  <Carousel autoplay>
  <div class="hero">
-    <div class="cube"></div>
-    <div class="cube"></div>
-    <div class="cube"></div>
-    <div class="cube"></div>
-    <div class="cube"></div>
-    <div class="cube"></div>
+    <div className="cube"></div>
+    <div className="cube"></div>
+    <div className="cube"></div>
+    <div className="cube"></div>
+    <div className="cube"></div>
+    <div className="cube"></div>
     <div className="header"/>
-    <div class = "Image">
+    <div className= "Image">
     <img src={require('../assets/infin8logofrowebsite.png')} alt=""/>
     </div>
     <div class="wrapper">
@@ -27,7 +27,16 @@ render() {
                         class="letter">i</span><span class="letter">i</span><span class="letter">t</span><span class="letter"> </span><span class="letter">b</span><span class="letter">a</span><span class="letter">n</span><span class="letter">g</span><span class="letter">a</span>
                         <span
                             class="letter">l</span><span class="letter">o</span><span class="letter">r</span><span class="letter">e</span></div>
-        </div>          
+        </div>
+        <br />
+        <div class = "wrapper2">
+          <div class = "numbers"> </div>
+           <span class = "letter">7</span><span class = "letter">F</span><span class = "letter">E</span><span class = "letter">B</span><span class = "letter">-</span><span class = "letter">2</span><span class = "letter">0</span><span class = "letter">2</span><span class = "letter">0</span><br />
+        </div>
+        <div class = "wrapper3">      
+           <div class = "numbers"></div>
+           <span class = "letter">8</span><span class = "letter">F</span><span class = "letter">E</span><span class = "letter">B</span><span class = "letter">-</span><span class = "letter">2</span><span class = "letter">0</span><span class = "letter">2</span><span class = "letter">0</span><br />
+        </div>
   </div>
   </Carousel>
  );
