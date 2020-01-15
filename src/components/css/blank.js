f995f6bd1028a1dbc42b8a2d1c240429211ccefb
jishnuvk <jish2000@gmail.com>
2020-01-15 13:18:18 +0530

    add animations for hexagons

---
+++ src/components/css/blank.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Carousel } from 'antd';
+import '../css/blank.css'
+import {ParallaxBanner} from 'react-scroll-parallax';
+
+class Blank extends React.Component {
+    render() {
+        return(
+            <div>
+            <div className="vertical">
+            <div className= "bannerContainer">
+                <ParallaxBanner
+                    className=" bannerBg"
+                    layers={[
+                        {
+                            image: require("../assets/dance1.jpg"),
+                            amount: 0.5,
+                        },
+
+                    ]}
+
+                    style = {{ height : '100vh'}}
+                />
+                
+            </div>
+        </div>
+        <div>
+        <h1> Event !! </h1>
+        <p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    </div>
+
+            </div>
+
+        );
+    }
+}
+export default Blank;
