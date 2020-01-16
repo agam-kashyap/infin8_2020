a81f575d30511cb1092bc8ad05e8c918d9379a63
agam-kashyap <agamkashyap2018@gmail.com>
2020-01-17 02:22:26 +0530

    about section updated, Background changed, iiitb logo changed

--- src/components/sections/AboutSection.js
+++ src/components/sections/AboutSection.js
@@ -8,14 +8,14 @@ class About extends React.Component {
             <br />
            <div className="outer-container">
                 <div className="image-container">
-                    <img className = "containImage" src={require('../assets/dance1.jpg')} alt="infin8"/>
+                    <img className = "containImage" src={require('../assets/about2.jpg')} alt="infin8"/>
                 </div>
                 <div className="text-container">
                     <p>Infin8 is the annual Cultural Fest of IIIT Bangalore. The
 fifth iteration of Infin8 has numerous live events,
 workshops and a multitude of contests, with the main
 attractions being the Battle of Bands and EDM Night
-featuring Candice Redding & Kalpanik Bass.</p>
+featuring prominent DJs of the country</p>
                 </div>
            </div>
            </div>
