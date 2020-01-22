a76836f3721b80dad82c413fa358959842910191
agam-kashyap <agamkashyap2018@gmail.com>
2020-01-22 12:55:27 +0530

    league of fanatics updated

--- src/components/sections/League.js
+++ src/components/sections/League.js
@@ -38,7 +38,21 @@ class League extends React.Component {
         <div className = "content">
             <p>Watching Anime, movies and reading novels all night long, only to get scolded by your parents "are they going to question you in these?" Well....yes! League Of Fanatics, an event where one's knowledge on your favourite novel/show is more valued than whether or not trump has truly been impeached and where otakus can finally shine (other than when they are isekaied of course 🙄)</p>
             <h2><ul type="disc"><li><b>Prize Money: &#8377;3500</b></li></ul></h2>
-        <h2><ul type="disc"><li><b>Spot Registration will be applicable.</b></li></ul></h2>        
+            <h2>Rules and Regulations</h2>
+        <ul type = "disc">
+            <li>Every team can have at least 1 and at most  4 members.</li>
+            <li>The first round will be a screening round, after which 8 teams will be selected.</li>
+            <li>Further details about the rounds will be told on the day of the event.</li>
+            <li>The decision of the judges will be absolute and binding.</li>
+            <li>Each participant should bring his/her valid college Id , otherwise your team will be disqualified.</li>
+            <li>Any team caught cheating will suffer immediate disqualification..</li>
+        </ul>
+        <h2><ul type="disc"><li><b>Registration deadline: 4th February 2020</b></li></ul></h2>
+            <br/>
+            <br/>
+            <br/>
+            
+            <a href = "https://docs.google.com/forms/d/1RzqxzdidIHKhtJBIRVaTi4L2jYTBwdTF6eNufl89N10/viewform?edit_requested=true"><button class = "button button1">Register</button></a>
         </div>
                     <FooterNoMap/>
             </div>
